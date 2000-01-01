b11167ae7e7845310b2bfcd3c045ec61daefb393
Marcus Leivo <marcus.leivo@github.mail.kapsi.fi>
2000-01-01 00:00:00 +0000

    More work
    On branch main
    Changes to be committed:
    	modified:   shape.h

--- shape.h
+++ shape.h
@@ -11,14 +11,45 @@ class Shape;
 template<typename T>
 concept AcceptedTypeForShape = not std::is_same_v<std::remove_cvref_t<T>, Shape> && not std::is_pointer_v<T>;
 
-struct Castable
+template<typename To, typename From>
+To cast(const From* from)
+{
+    return from->template cast<To>();
+}
+
+template<typename T>
+class Castable
 {
     template<typename To, typename From>
     friend To cast(const From* from);
+
+private:
+    template<typename To>
+    To cast() const
+    {
+        auto* derived = static_cast<const T*>(this);
+        using cleanT = std::remove_cvref_t<std::remove_pointer_t<To>>;
+        if (auto* model = dynamic_cast<typename T::template Model<cleanT>*>(derived->m_impl.get()))
+        {
+            return &model->m_t;
+        }
+        if (auto* model = dynamic_cast<typename T::template Model<cleanT&>*>(derived->m_impl.get()))
+        {
+            return &model->m_t;
+        }
+        if (auto* model = dynamic_cast<typename T::template Model<const cleanT&>*>(derived->m_impl.get()))
+        {
+            return &model->m_t;
+        }
+        //                static_assert(std::is_same_v<T, cleanT> && false);
+        return nullptr;
+    }
 };
 
-class Shape : public Castable
+class Shape : public Castable<Shape>
 {
+    friend class Castable<Shape>;
+
 public:
     template<AcceptedTypeForShape T>
     explicit Shape(T&& t) : m_impl(std::make_unique<Model<T>>(std::forward<T>(t)))
@@ -30,7 +61,7 @@ public:
         shape.m_impl->doDraw();
     }
 
-    // private:
+private:
     struct Concept
     {
         virtual ~Concept() = default;
@@ -95,24 +126,4 @@ private:
 void draw(const Square& square)
 {
     std::cout << "Square with side " << square.side() << std::endl;
-}
-
-template<typename To, typename From>
-To cast(const From* from)
-{
-    using cleanT = std::remove_cvref_t<std::remove_pointer_t<To>>;
-    if (auto* model = dynamic_cast<typename From::template Model<cleanT>*>(from->m_impl.get()))
-    {
-        return &model->m_t;
-    }
-    if (auto* model = dynamic_cast<typename From::template Model<cleanT&>*>(from->m_impl.get()))
-    {
-        return &model->m_t;
-    }
-    if (auto* model = dynamic_cast<typename From::template Model<const cleanT&>*>(from->m_impl.get()))
-    {
-        return &model->m_t;
-    }
-    //                static_assert(std::is_same_v<T, cleanT> && false);
-    return nullptr;
 }
